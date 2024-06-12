79755fc78d45562f443f812b42e928877ee8b9b2
David Shaw <dshaw11@utk.edu>
2024-06-12 14:35:14 -0400

    currently successfully taking in a string prompt from a user and checking agains currently available commands

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 type cliCommand struct {
 	name string
@@ -20,9 +25,43 @@ func main() {
 			name: "exit",
 			description: "Exit the Pokedex",
 			callback: commandExit,
+		},
+	}
+	// fmt.Println(commands)
+	for {
+		userInput := userPrompt("Pokedex >")
+		if userInput == commands[userInput].name {
+			commands[userInput].callback()
+		} else {
+			fmt.Println("Invalid Command. Please Try Again")
+			fmt.Println("To see available options, enter help")
 		}
 	}
-	i := fmt.Print("Pokedex > ")
-	fmt.Scan(&i)
-	fmt.Println(i)
+}
+
+func userPrompt(label string) string {
+	var s string
+	r := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Fprint(os.Stderr, label+" ")
+		s, _ = r.ReadString('\n')
+		if s != "" {
+			break
+		}
+	}
+	return strings.TrimSpace(s)
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Commands Available:")
+	fmt.Println("help: Displays this help message")
+	fmt.Println("exit: Exit the Pokedex")
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Goodbye!")
+	os.Exit(0)
+	return nil
 }
