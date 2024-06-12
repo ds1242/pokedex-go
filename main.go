d0fa782c35d92a8374283ed3fbf4bdfefb8f1bd5
David Shaw <dshaw11@utk.edu>
2024-06-12 13:16:42 -0400

    started to setup the possible cli commands

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
-// import "fmt"
+import "fmt"
+
+type cliCommand struct {
+	name string
+	description string
+	callback func() error
+}
 
 
 func main() {
-	
+	commands := map[string]cliCommand {
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+		"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		}
+	}
+	i := fmt.Print("Pokedex > ")
+	fmt.Scan(&i)
+	fmt.Println(i)
 }
