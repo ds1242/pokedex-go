1b23df5f9c8f3b42a2ed95dd2c8f7d3451d626cb
David Shaw <dshaw11@utk.edu>
2024-06-13 09:49:11 -0400

    setup get request to get data from endpoint

--- command_map.go
+++ command_map.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
 
 func commandMap() error {
-	fmt.Println("map")
+	res, err := http.Get("https://pokeapi.co/api/v2/location")
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", body)
 	return nil
 }
