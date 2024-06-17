28c84864917f8a4515deb99b89924c1deb4eba7f
David Shaw <dshaw11@utk.edu>
2024-06-17 12:53:24 -0400

    created intital tests provided

---
+++ pokecache/pokecache.go
@@ -0,0 +1,17 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+type Cache struct {
+	data     map[string]cacheEntry
+	mutex    sync.Mutex
+	interval time.Duration
+}
