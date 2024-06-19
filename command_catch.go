b16e65579a46d78c2839700348aa2d7816216598
David Shaw <dshaw11@utk.edu>
2024-06-19 12:27:11 -0400

    created a pokemon type list function to get pokemon information

---
+++ command_catch.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func commandCatch(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("no pokemon name provided")
+	}
+
+	
+}
