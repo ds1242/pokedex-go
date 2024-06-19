bb134641db5e7f76e01304af65c8438505da6668
David Shaw <dshaw11@utk.edu>
2024-06-19 13:25:48 -0400

    fixed some formatting and error handling

--- command_inspect.go
+++ command_inspect.go
@@ -6,11 +6,11 @@ import (
 )
 
 func commandInspect(cfg *config, args ...string) error {
-	if len(args) != 1 {
-		return errors.New("no pokemon name provided to inspect")
-	}
 	if len(cfg.caughtPokemon) == 0 {
 		return errors.New("you have not caught any pokemon")
+	}
+	if len(args) != 1 {
+		return errors.New("no pokemon name provided to inspect")
 	}
 
 	pokemon, ok := cfg.caughtPokemon[args[0]]
@@ -23,7 +23,7 @@ func commandInspect(cfg *config, args ...string) error {
 	fmt.Printf("Weight: %d\n", pokemon.Weight)
 	fmt.Println("Stats:")
 	for _, stat := range pokemon.Stats {
-		fmt.Printf("-%s: %d", stat.Stat.Name, stat.BaseStat)
+		fmt.Printf("-%s: %d\n", stat.Stat.Name, stat.BaseStat)
 	}
 	for _, typeInfo := range pokemon.Types {
 		fmt.Printf(" - %s\n", typeInfo.Type.Name)
