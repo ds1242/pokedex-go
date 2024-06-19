6b19cb92e6af15361c5d59b056ebd93af4a8f373
David Shaw <dshaw11@utk.edu>
2024-06-19 12:36:24 -0400

    initial setup for catch function

--- command_catch.go
+++ command_catch.go
@@ -3,12 +3,28 @@ package main
 import (
 	"errors"
 	"fmt"
+	"math/rand/v2"
 )
 
 func commandCatch(cfg *config, args ...string) error {
 	if len(args) != 1 {
 		return errors.New("no pokemon name provided")
 	}
-
 	
+	pokemonName := args[0]
+
+	resp, err := cfg.pokeapiClient.GetPokemonInfo(pokemonName)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Throwing a Pokeball at %s\n", resp.Name)
+	playerVal := rand.IntN(2 * resp.BaseExperience)
+	if playerVal > resp.BaseExperience {
+		fmt.Printf("%s was caught!", resp.Name)
+	} else {
+		fmt.Printf("%s escaped!", resp.Name)
+	}
+	return nil
 }
